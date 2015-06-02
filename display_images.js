9b6062499d0b65bf81a664a4afd2fc10f02dc1f0
minkaart <theartist@minkaart.net>
2015-06-03 00:11:17 +0800

    ts

--- display_images.js
+++ display_images.js
@@ -157,6 +157,7 @@ pageload();
 		//function dynamically populates imageObjects based on image JSON file based on files in "images" folder and displays the images scrolling - to only display the images and manually populate imageObject, use initiatepage() alone with imageObject
 		function popimageArray(){
 			$.getJSON("get_images.php").done(function(data){
+				console.log(data);
 				$.each(data, function(key, val){
 					var imageObject = {
 						"image_ref" : "https://euroclouds.s3.amazonaws.com/images/"+key,
