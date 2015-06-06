3250f4473d3e715a13e6633cec743b4fb6e9da33
minkaart <theartist@minkaart.net>
2015-06-06 14:45:26 +0800

    bugs in first 30

--- get_images.php
+++ get_images.php
@@ -10,7 +10,7 @@ $data = array('total' => 0, 'first_30' => "", 'more' => false, 'error' => "error
 try {
 	$image_json = file_get_contents('s3://'.$bucket.'/image_JSON.json');
 } catch(Exception $e){
-	$data[error] = $data[error].$e;
+	$data[error] = $data[error].$_POST['start_val'];
 	}
 
 
@@ -18,8 +18,10 @@ try {
 $images = json_decode($image_json, true);
 $data[total] = count($images); 
 
+$data[first_30] = $image_json;
 
-if ($data[total] > 30) {
+
+/*if ($data[total] > 30) {
 	if($_POST['start_val']){
 		$length = $_POST['start_val'] * 30; 
 		$data[error] = $data[error]."assessed length is ".$length;
@@ -39,7 +41,7 @@ if ($data[total] > 30) {
 else {
 	$data[first_30] = $image_json;
 	$data[more] = false;
-}
+}*/
 
 $data = json_encode($data);
 echo $data;
