ee269ea0cb564b595057d899e926248a32e0e9f0
minkaart <theartist@minkaart.net>
2015-06-10 18:03:38 +0800

    trying something

--- display_images.js
+++ display_images.js
@@ -311,7 +311,7 @@
 			//adjust display
 			$(".stop_pause").hide();
 			$("#start_button").show();
-			$(".launch").off("click");
+			//$(".launch").off("click");
 
 			//check for ani_running at 1, 2, or 3
 			if(ani_running%2 !== 0 && ani_running !== 0){ //if ani_running is 1 or 3 and not 0
